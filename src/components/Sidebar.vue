1b64b0743041c220e973b40a093a0a6b575c9c79
RevaPrasetya <revabro3@gmail.com>
2024-01-02 01:59:57 +0800

    fix sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -13,6 +13,7 @@ const logout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("user");
     this.$router.push({ path: '/login' })
+    isLoggedIn = false;
 };
 </script>
 
